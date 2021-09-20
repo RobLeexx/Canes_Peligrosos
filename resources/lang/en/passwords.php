505751b775922f3de536dc06f72a515e71825bba
RobLeexx <roblee99.mc@gmail.com>
2021-09-20 10:10:20 -0400

    idiomaPHPvalidaciones

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,9 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'reset' => 'Tu contraseña fue reestablecida!',
+    'sent' => 'Enviamos tu contraseña al correo electrónico.',
+    'throttled' => 'Espere unos segundos para reintentar.',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user' => "No se encontró un usuario con esa dirección de correo electrónico.",
 ];
