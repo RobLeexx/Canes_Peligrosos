062b2c18c29e9328f78366079ecfabde660b5fed
RobLeexx <roblee99.mc@gmail.com>
2021-10-02 13:26:14 -0400

    CameraPreSave

--- resources/js/Pages/requisitos.vue
+++ resources/js/Pages/requisitos.vue
@@ -307,32 +307,99 @@
                                                 outlined
                                                 ></v-text-field>
                                             </v-col>
-                                            <v-col
-                                                cols="12"
-                                                sm="6"
-                                            >
+                                            <v-col cols="12" lg="6" sm="6">
                                                 <template>
-                                                <v-file-input style="height: 45px"
-                                                    outlined
-                                                    dense
-                                                    show-size
-                                                ><template v-slot:label>
-                                                    <div>
-                                                    Foto o escaneo del Documento<small> (opcional)</small>
-                                                    </div>
-                                                    </template></v-file-input>
-                                                <v-subheader style="
-                                                    display: flex;
-                                                    align-items: flex-start;
-                                                    justify-content: flex-end;">
-                                                    *Solo se admite un elemento</v-subheader>
+                                                    <v-file-input style="height: 45px"
+                                                        outlined
+                                                        dense
+                                                        show-size
+                                                    ><template v-slot:label>
+                                                        <div>
+                                                        Foto o escaneo del Documento<small> (opcional)</small>
+                                                        </div>
+                                                        </template></v-file-input>
+                                                    <v-subheader style="
+                                                        display: flex;
+                                                        align-items: flex-start;
+                                                        justify-content: flex-end;">
+                                                        *Solo se admite un elemento</v-subheader>
                                                 </template>
+                                            </v-col>
+                                            <v-col cols="12" lg="1" sm="1" style="display: flex; justify-content: center; padding: 0">
+                                                <v-dialog v-model="dialog" width="50%" height="100%">
+                                                    <template v-slot:activator="{ on, attrs }">
+                                                        <v-btn fab color="primary" v-bind="attrs" v-on="on" @click="toggleCamera" :loading="dialog">
+                                                            <v-icon>
+                                                                mdi-camera-plus
+                                                            </v-icon>
+                                                        </v-btn>
+                                                    </template>
+                                                    <v-card>
+                                                        <v-card-title class="text-h5 grey lighten-2" style="display: flex; justify-content: center">
+                                                            Foto Frontal del Propietario
+                                                        </v-card-title>
+
+                                                        <div v-show="isCameraOpen && isLoading">
+                                                            <ul>
+                                                            <li></li>
+                                                            <li></li>
+                                                            <li></li>
+                                                            </ul>
+                                                        </div>
+                                                        
+                                                        <v-row style="padding:12px; margin-right: 5px">
+                                                            <div v-if="isCameraOpen" v-show="!isLoading" :class="{ 'flash' : isShotPhoto }">
+                                                                <div :class="{'flash' : isShotPhoto}"></div>
+                                                                <video v-show="!isPhotoTaken" ref="camera" :width="450" :height="337.5" autoplay></video>
+                                                                <canvas v-show="isPhotoTaken" id="photoTaken" ref="canvas" :width="450" :height="337.5"></canvas>
+                                                            </div>
+                                                            
+                                                            <v-row style="display: flex; flex-direction: column">
+                                                                <div v-if="isCameraOpen && !isLoading && !isPhotoTaken" style="text-align: center; padding: 50px">
+                                                                    <v-btn fab color="success" @click="takePhoto">
+                                                                        <v-icon>
+                                                                            mdi-camera
+                                                                        </v-icon>
+                                                                    </v-btn>
+                                                                </div>
+                                                                <div v-if="isCameraOpen && !isLoading && isPhotoTaken" style="text-align: center; padding: 50px">
+                                                                    <v-btn fab color="warning" @click="takePhoto">
+                                                                        <v-icon>
+                                                                            mdi-reload
+                                                                        </v-icon>
+                                                                    </v-btn>
+                                                                </div>
+                                                                
+                                                                <div v-if="isPhotoTaken && isCameraOpen" style="padding: 50px; text-align: center">
+                                                                    <a id="downloadPhoto" download="my-photo.jpg" class="button" role="button" @click="downloadImage">
+                                                                        <v-icon color="primary">
+                                                                            mdi-download
+                                                                        </v-icon>
+                                                                        Descargar
+                                                                    </a>
+                                                                </div>
+                                                            </v-row>
+                                                        </v-row>
+
+                                                        <v-divider></v-divider>
+                                                        <v-card-actions>
+                                                        <v-spacer></v-spacer>
+                                                        <v-btn style="padding: 10px" color="error" @click.prevent="dialog = false" @click="toggleCamera">
+                                                            Cerrar
+                                                        </v-btn>
+                                                        </v-card-actions>
+                                                    </v-card>
+                                                    </v-dialog>
+                                            </v-col>
+                                            <v-col cols="12" lg="5" md="5" sm="5">
                                                 <template>
-                                                <v-file-input
-                                                    label="Foto del Propietario"
-                                                    outlined
-                                                    prepend-icon="mdi-camera"
-                                                ></v-file-input>
+                                                    <v-file-input
+                                                        label="Foto del Propietario"
+                                                        outlined
+                                                        dense
+                                                        show-size
+                                                        prepend-icon="mdi-face"
+                                                    ></v-file-input>
                                                 </template>
                                             </v-col>
                                             </v-row>
@@ -482,6 +549,12 @@
             menuMemo: false,
             menuProp: false,
             doc: ['CI', 'RUT', 'Pasaporte'],
+            dialog: false,
+            isCameraOpen: false,
+            isPhotoTaken: false,
+            isShotPhoto: false,
+            isLoading: false,
+            link: '#'
             }
             
         },
@@ -540,6 +613,67 @@
             save (dateProp) {
                 this.$refs.menuProp.save(dateProp)
             },
+            toggleCamera() {
+                if(this.isCameraOpen) {
+                    this.isCameraOpen = false;
+                    this.isPhotoTaken = false;
+                    this.isShotPhoto = false;
+                    this.stopCameraStream();
+                } else {
+                    this.isCameraOpen = true;
+                    this.createCameraElement();
+                }
+            },
+            createCameraElement() {
+                this.isLoading = true;
+                
+                const constraints = (window.constraints = {
+                            audio: false,
+                            video: true
+            });
+            navigator.mediaDevices
+                        .getUserMedia(constraints)
+                        .then(stream => {
+                this.isLoading = false;
+                            this.$refs.camera.srcObject = stream;
+                        })
+                        .catch(error => {
+                this.isLoading = false;
+                            alert("May the browser didn't support or there is some errors.");
+                        });
+            },
+                        stopCameraStream() {
+                let tracks = this.$refs.camera.srcObject.getTracks();
+
+                        tracks.forEach(track => {
+                            track.stop();
+                        });
+                },
+                
+                takePhoto() {
+                if(!this.isPhotoTaken) {
+                    this.isShotPhoto = true;
+
+                    const FLASH_TIMEOUT = 50;
+
+                    setTimeout(() => {
+                    this.isShotPhoto = false;
+                    }, FLASH_TIMEOUT);
+                }
+                
+                this.isPhotoTaken = !this.isPhotoTaken;
+                
+                const context = this.$refs.canvas.getContext('2d');
+                context.drawImage(this.$refs.camera, 0, 0, 450, 337.5);
+                },
+                
+                downloadImage() {
+                const download = document.getElementById("downloadPhoto");
+                const canvas = document.getElementById("photoTaken").toDataURL("image/jpeg")
+                .replace("image/jpeg", "image/octet-stream");
+                download.setAttribute("href", canvas);
+                }
+
         },
         props: 
         {
