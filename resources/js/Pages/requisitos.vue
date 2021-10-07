cc44c678aa22742589b3ad16eed9e6d9d4c972f8
RobLeexx <roblee99.mc@gmail.com>
2021-10-06 20:42:00 -0400

    PreAntecedentes1

--- resources/js/Pages/requisitos.vue
+++ resources/js/Pages/requisitos.vue
@@ -488,7 +488,76 @@
                                     color="grey lighten-1"
                                     class="mb-12"
                                     height="200px"
-                                ></v-card>
+                                >
+                                <template>
+                                    <v-card
+                                        flat
+                                        tile
+                                    >
+                                        <v-window
+                                        v-model="onboarding"
+                                        reverse
+                                        >
+                                        <v-window-item
+                                            v-for="n in length"
+                                            :key="`card-${n}`"
+                                        >
+                                            <v-card
+                                            color="grey"
+                                            height="200"
+                                            >
+                                            <v-row
+                                                class="fill-height"
+                                                align="center"
+                                                justify="center"
+                                            >
+                                                <h1
+                                                style="font-size: 5rem;"
+                                                class="white--text"
+                                                >
+                                                Slide {{ n }}
+                                                </h1>
+                                            </v-row>
+                                            </v-card>
+                                        </v-window-item>
+                                        </v-window>
+
+                                        <v-card-actions class="justify-space-between">
+                                        <v-btn
+                                            text
+                                            @click="prev"
+                                        >
+                                            <v-icon>mdi-chevron-left</v-icon>
+                                        </v-btn>
+                                        <v-item-group
+                                            v-model="onboarding"
+                                            class="text-center"
+                                            mandatory
+                                        >
+                                            <v-item
+                                            v-for="n in length"
+                                            :key="`btn-${n}`"
+                                            v-slot="{ active, toggle }"
+                                            >
+                                            <v-btn
+                                                :input-value="active"
+                                                icon
+                                                @click="toggle"
+                                            >
+                                                <v-icon>mdi-record</v-icon>
+                                            </v-btn>
+                                            </v-item>
+                                        </v-item-group>
+                                        <v-btn
+                                            text
+                                            @click="next"
+                                        >
+                                            <v-icon>mdi-chevron-right</v-icon>
+                                        </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </template>
+                                </v-card>
                                 <v-btn
                                     color="primary"
                                     @click="e6 = 4"
@@ -607,7 +676,9 @@
             isPhotoTaken: false,
             isShotPhoto: false,
             isLoading: false,
-            link: '#'
+            link: '#',
+            length: 3,
+            onboarding: 0
             }
             
         },
@@ -717,8 +788,19 @@
                 const canvas = document.getElementById("photoTaken").toDataURL("image/jpeg")
                 .replace("image/jpeg", "image/octet-stream");
                 download.setAttribute("href", canvas);
-                }
+                },
 
+            /* Antecedentes */
+            next () {
+                this.onboarding = this.onboarding + 1 === this.length
+                ? 0
+                : this.onboarding + 1
+            },
+            prev () {
+                this.onboarding = this.onboarding - 1 < 0
+                ? this.length - 1
+                : this.onboarding - 1
+            },
         },
         props: 
         {
