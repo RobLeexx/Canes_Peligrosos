cc936178c54ed8dc7263f50b793707de9e97f421
RobLeexx <roblee99.mc@gmail.com>
2022-01-17 21:56:18 -0400

    PruebaDocker1.1

--- resources/js/Pages/capacitaciones.vue
+++ resources/js/Pages/capacitaciones.vue
@@ -6,12 +6,25 @@
             </h2>
         </template>
 
+        <v-app>
         <div class="py-12" style="background: #33691E">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg" style="box-shadow: 0px 0px 30px">
+                <div style="padding-left: 10px; position: absolute; top: 10px">
+                    <v-btn 
+                        fab
+                        color="primary"
+                        :href="route('registros.create')"
+                        ><v-icon>mdi-plus</v-icon>
+                    </v-btn>
                 </div>
+                <v-card style="box-shadow: 0px 0px 30px; border-radius: 10px; position: initial">
+                    <v-card-title style="padding: 0; padding-inline: 20px">
+                        Prueba
+                    </v-card-title>
+                </v-card>
             </div>
         </div>
+        </v-app>
     </app-layout>
 </template>
 
